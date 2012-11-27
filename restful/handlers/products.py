4513473f93d6aa8296eb3e97c47f47d358b498b3
Tulio Ruiz <lucipher.80@gmail.com>
2012-11-26 22:18:23 -0430

    Metodos faltantes

--- restful/handlers/products.py
+++ restful/handlers/products.py
@@ -4,58 +4,39 @@ from utils import (json_handler, copyListDicts)
 import psycopg2
 import psycopg2.extras
 import json
+import logging
+
+gen_log = logging.getLogger("tornado.general")
 
 class ProductHandler(tornado.web.RequestHandler):
-    """
-    Clase para el manejo de productos, es la unica que tiene implementada 
-    todos los metodos rest para las pruebas, el resto de las clases 
-    se hacen de manera similar, para evitar exceso de documentacion unicamente
-    se implementan _todos_ los metodos aca
-    """
+
     @property
     def cursor(self):
-        """
-        Se usa un unico cursor instanciado cuando se crea la aplicacion
-        """
         return self.application.cur
 
     def _get_products(self, product_ids = None):
-        """
-        Este metodo retorna una lista de productos segun el valor de product_ids
-        no es necesartio hacerlo siempre de esta manera, pero la idea es ilustrar 
-        que se pueden ejecutar metodos externos al get o a cualquier otro
-        de los metodos rest
-        """
         if product_ids:
             self.cursor.execute("SELECT * FROM products WHERE id IN (%s)", (product_ids, ))
         else:
             self.cursor.execute("SELECT * FROM products")
         res = copyListDicts( self.cursor.fetchall())
         ret = {}
-        
-        """
-        Como lo indica la convencion REST las referencias a otros objetos debe ser enviada
-        como una propiedad, aunque en este caso es una referencia al mismo objeto, sin embargo
-        es lo recomendado para no verse en la oblicacion de construirlo del lado del cliente.
-        La propiedad self.request.host se usa para obtener la url a la que se realizo la peticion
-        en caso de que exista un proxy o cualquier intermedio
-        """
         for r in res:
             r.update({'ref' : 'http://%s/products/%s'%(self.request.host, r.get('id'))})
         ret.update({'products':res})
         return  ret
 
+    def _send_response(self, res):
+        self.set_header("Content-Type", "application/json")
+        self.write(json.dumps(res))
+        self.finish()
+        
     def get(self, product_id):
         if product_id:
             res = self._get_products(product_id)
         else:
             res = self._get_products()
-        """
-        La funcion dumps de la libreria json serializa un diccionario python 
-        """
-        self.set_header("Content-Type", "application/json")
-        self.write(json.dumps(res))
-        self.finish()
+        self._send_response(res)
 
     def post(self, p):
         name = self.get_argument('name', '')
@@ -64,26 +45,46 @@ class ProductHandler(tornado.web.RequestHandler):
         price = self.get_argument('price', 0)
         product_id = self.get_argument('id')
         fields = self.request.arguments.keys() 
-        fo
-        if product_id:
-            self.cursor.execute("""UPDATE products 
-                                    SET name = %s, 
-                                    quantity = %s, 
-                                    code = %s, 
-                                    price = %s
-                                    WHERE id = %s""",
-                                (name, quantity, code, price, product_id))
-        
+        print fields
         res = {'status' : {'id' : 'OK', 'message' : ''}}
-        self.set_header("Content-Type", "application/json")
-        self.write(json.dumps(res))
-        self.finish()
+       
+        if product_id:
+            try:
+                self.cursor.execute("""UPDATE products 
+                                        SET name = %s, 
+                                        quantity = %s, 
+                                        code = %s, 
+                                        price = %s
+                                        WHERE id = %s""",
+                                    (name, quantity, code, price, product_id))
+            except Exception as e:
+                res.update({'status':{'id' : 'ERROR', 'message' : 'Hubo un error, no se pudo actualziar el registro'}})
+                gen_log.error("Error actualizando el registro", exc_info=True)
+        self._send_response(res)
         
     def delete(self, p):
-            # self.cursor.execute("""INSERT INTO products (name, quantity, code, price) 
-                                    # VALUES (%s, %s, %s, %s)""",
-                                # (name, quantity, code, price))
-        print p
+        product_id = self.get_argument('id')
+        res = {'status' : {'id' : 'OK', 'message' : ''}}
+        try:
+            self.cursor.execute("""DELETE FROM products 
+                                  WHERE id = %s""",
+                                    (product_id,))
+        except Exception as e:
+            res.update({'status':{'id' : 'ERROR', 'message' : 'Hubo un error, no se pudo eliminar el registro'}})
+            gen_log.error("Error eliminando el registro", exc_info=True)
+        self._send_response(res)        
         
     def put(self, p):
-        print p
+        name = self.get_argument('name', '')
+        quantity = self.get_argument('quantity', 0)
+        code = self.get_argument('code')
+        price = self.get_argument('price', 0)
+        res = {'status' : {'id' : 'OK', 'message' : ''}}
+        try:
+            self.cursor.execute("""INSERT INTO products (name, quantity, code, price) 
+                                  VALUES (%s, %s, %s, %s)""",
+                                    (name, quantity, code, price))
+        except Exception as e:
+            res.update({'status':{'id' : 'ERROR', 'message' : 'Hubo un error, no se pudo crear el registro'}})
+            gen_log.error("Error agregando el registro", exc_info=True)
+        self._send_response(res)
